72ff04cd6ac60b2c48020c034b2ce06583db0a9f
bvs3040 <taga-si@mail.ru>
2022-05-03 19:35:01 +0700

    task_2 correct

--- task_2/task_2.js
+++ task_2/task_2.js
@@ -1,6 +1,12 @@
-// {"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
 
-const jsonString = `{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}`;
-const unit = JSON.parse(jsonString);
-const result = JSON.stringify(unit);
+const obj = {
+    name : "Anton",
+    age : 36,
+    skills : ["Javascript", "HTML", "CSS"],
+    salary : 80000
+};
+
+const result = JSON.stringify(obj);
 console.log(result);
+
+// {"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
